0de99f53bb48d33f6c056b9ee792e0be1574f436
Soloman Garcia <soloman.garcia@gmail.com>
2020-04-13 13:42:15 -0600

    work on making functional

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
 const formatMessage = require("./utils/messages");
-const { userJoin, getCurrentUser } = require('./utils/users');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -29,6 +29,12 @@ io.on("connection", (socket) => {
       "message",
       formatMessage(botName, `${user.username} has joined the chat`)
     );
+
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    });
   });
 
   // Listen for chatMessage
@@ -40,7 +46,18 @@ io.on("connection", (socket) => {
 
   // Runs when a client disconnects
   socket.on("disconnect", () => {
-    io.emit("message", formatMessage(botName, "A user has left the chat"));
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", formatMessage(botName, `${user.username} has left the chat`));
+
+      // Send users and room info
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
+    }
+  
   });
 });
 
