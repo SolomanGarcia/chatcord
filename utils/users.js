d444d5eb7e0461ea3df4500b944466122ac37d04
Soloman Garcia <soloman.garcia@gmail.com>
2020-04-13 13:23:53 -0600

    add getRoomUsers function

--- utils/users.js
+++ utils/users.js
@@ -24,8 +24,13 @@ function userLeave(id) {
 };
 
 // Get room users
+function getRoomUsers(room) {
+  return users.filter(user => user.room === room);
+};
 
 module.exports = {
   userJoin,
-  getCurrentUser
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
 };
