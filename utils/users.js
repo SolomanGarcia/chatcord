33065202d6eee096e9b97264ecd8b805b4a1e559
Soloman Garcia <soloman.garcia@gmail.com>
2020-04-13 13:21:11 -0600

    add userLeave function

--- utils/users.js
+++ utils/users.js
@@ -14,6 +14,17 @@ function getCurrentUser(id) {
   return users.find(user => user.id === id)
 };
 
+// User leaves
+function userLeave(id) {
+  const index = users.findIndex(user => user.id === id);
+
+  if (index !== -1) {
+    return users.splice(index, 1);
+  }
+};
+
+// Get room users
+
 module.exports = {
   userJoin,
   getCurrentUser
