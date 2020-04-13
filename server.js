c6ec43eea99ae22bda63c2566685f0d82d272d15
Soloman Garcia <soloman.garcia@gmail.com>
2020-04-13 13:05:16 -0600

    create users.js

--- server.js
+++ server.js
@@ -1,35 +1,39 @@
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketio = require('socket.io');
-const formatMessage = require('./utils/messages');
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-const botName = 'ChatCord Bot';
+const botName = "ChatCord Bot";
 
 // Run when client connects
-io.on('connection', socket => {
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    // Welcome current user
+    socket.emit("message", formatMessage(botName, "Welcome to ChatCord!"));
 
-  // Welcome current user
-  socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
-
-  // Broadcast when a user connects
-  socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
-
-  // Runs when a client disconnects
-  socket.on('disconnect', () => {
-    io.emit('message', formatMessage(botName, 'A user has left the chat'));
+    // Broadcast when a user connects
+    socket.broadcast.emit(
+      "message",
+      formatMessage(botName, "A user has joined the chat")
+    );
   });
 
   // Listen for chatMessage
-  socket.on('chatMessage', (msg) => {
-    io.emit('message', formatMessage('USER', msg));
+  socket.on("chatMessage", (msg) => {
+    io.emit("message", formatMessage("USER", msg));
+  });
+
+  // Runs when a client disconnects
+  socket.on("disconnect", () => {
+    io.emit("message", formatMessage(botName, "A user has left the chat"));
   });
 });
 
